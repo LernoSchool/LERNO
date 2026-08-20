f608c24505dc8d9b87ca66cc636343e42d2c1895
Ava Bayat <bayat.ava1393@gmail.com>
2026-08-20 13:37:11 +0330

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='lerno-v9-account-switch';
+const CACHE_NAME='lerno-v10-smart-install';
 const APP_FILES=['./','./index.html','./manifest.webmanifest','./assets/style.css','./assets/auth.js','./assets/app.js','./assets/lerno-logo-180-v2.png','./assets/lerno-logo-192-v2.png','./assets/lerno-logo-512-v2.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_FILES)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
